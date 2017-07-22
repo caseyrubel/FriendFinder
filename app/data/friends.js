d1af3f1b7b471bdf56470788f78687e203e02412
Casey Rubel <caseyrubel@gmail.com>
2017-07-22 09:17:40 -0700

    a

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+var userData = [{
+    "name": "Homer Simpson",
+    "photo": "https://tomtom.ssl.cdn.sdlmedia.com/image/636035800177516681RA.jpg",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}, {
+    "name": "Bart Simpson",
+    "photo": "https://static.simpsonswiki.com/images/thumb/6/65/Bart_Simpson.png/200px-Bart_Simpson.png",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}];
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = userData;
